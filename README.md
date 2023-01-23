894bd483a7cfda1aacf160c394f488b3aa46f394
Mahmoud Kamal <mahmoud@untap.us>
2023-01-24 01:23:09 +0200

    fix all issues

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ npm run build (build project fold in /build)
 npm run test (build and test project )
 
 node build/.
+
+## api endpoints
+### we have just one endpoint in this project 
+http://localhost:3000/api/images?filename=santamonica&width=100&height=100
--- src/utilities/resizeImage.ts
+++ src/utilities/resizeImage.ts
@@ -8,32 +8,38 @@ const resizeImage = async (
   res: express.Response,
   next: () => void
 ): Promise<void> => {
+
+  // get all query parameters from url (req)
   const width = Number(req.query.width) as unknown as number;
   const height = Number(req.query.height) as unknown as number;
   const imageName = req.query.filename as unknown as string;
   const imagePath = path.resolve('./assets/full') as unknown as string;
   const thumbnailPath = path.resolve('./assets/thumb') as unknown as string;
-  const resizedImage = thumbnailPath + `/${imageName}-${width}-${height}.jpg`;
+  const resizedImage = thumbnailPath + `/${imageName}-${width}-${height}.jpg` as unknown as string;
+  const requestedImage = `${imagePath}/${imageName}.jpg`
 
   // check image, width and height are in a valid formate
   // check image exist or make resize (cache)
-  if (!imageName) {
-    console.log('Image name is required');
-    res.send('Image name is required');
+  if(!fs.existsSync(requestedImage)){
+    console.error('Invalid input for filename ');
+    res.status(400).send('Invalid input for filename ');
+  } else if (!imageName) {
+    console.error('Image name is required');
+    res.status(400).send('Image name is required');
   } else if (isNaN(width) || !width) {
-    console.log('Please enter a valid number for width and should be > 0');
-    res.send('Please enter a valid number for width and should be > 0');
+    console.error('Please enter a valid number for width and should be > 0');
+    res.status(400).send('Please enter a valid number for width and should be > 0');
   } else if (isNaN(height) || !height) {
-    console.log('Please enter a valid number for height and should be > 0');
-    res.send('Please enter a valid number for height and should be > 0');
+    console.error('Please enter a valid number for height and should be > 0');
+    res.status(400).send('Please enter a valid number for height and should be > 0');
   }
   // check cached image exist or not
   else if (fs.existsSync(resizedImage)) {
     console.log('this file is already exist use the cached version');
-    res.sendFile(resizedImage);
+    res.status(200).sendFile(resizedImage);
   } else {
     await shrapResizeImage(imagePath, imageName, width, height, thumbnailPath);
-    res.sendFile(resizedImage);
+    res.status(200).sendFile(resizedImage);
   }
 
   next();
